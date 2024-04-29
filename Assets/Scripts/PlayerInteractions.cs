b9b03ebe425d333109b7fe90b7cdaefc9bb3e7b6
Ricardo Moctezuma <rmoctezumaf@gmail.com>
2024-04-28 22:06:22 -0500

    +GrabUI

--- Assets/Scripts/PlayerInteractions.cs
+++ Assets/Scripts/PlayerInteractions.cs
@@ -6,47 +6,61 @@ namespace StarterAssets
     {
         private GameObject mainCamera;
         private GameObject grabbedObject;
+        private GameObject grabUI;
 
         // Start is called before the first frame update
         void Start()
         {
             mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+            grabUI = GameObject.Find("GrabUI");
+            grabUI.SetActive(false);
+        }
+
+        void Update()
+        {
+            if (GrabCheck() != null)
+            {
+                grabUI.SetActive(true);
+            }
+            else
+            {
+                grabUI.SetActive(false);
+            }
+        }
+
+        GameObject GrabCheck()
+        {
+            // Sends a ray 3 meters out from the camera
+            if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.TransformDirection(Vector3.forward), out RaycastHit hit, 3f))
+            {
+                if (hit.collider.gameObject.CompareTag("Grabbable") && hit.collider.gameObject != grabbedObject)
+                {
+                    return hit.collider.gameObject;
+                }
+            }
+            return null;
         }
 
         void OnGrab()
         {
-            GrabCheck();
+            GrabAttempt();
         }
 
-        void GrabCheck()
+        void GrabAttempt()
         {
-            // Does the ray intersect any objects excluding the player layer
-            if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.TransformDirection(Vector3.forward), out RaycastHit hit, 3f))
+            var checkedObj = GrabCheck();
+            if (checkedObj != null)
             {
-                if (hit.collider.gameObject.CompareTag("Grabbable"))
-                {
-                    if (grabbedObject == null)
-                    {
-                        // Stores the hit object, reparents it to the camera, and turns off its physics
-                        grabbedObject = hit.collider.gameObject;
-                        grabbedObject.transform.SetParent(mainCamera.transform);
-                        grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
-                    }
-                    else
-                    {
-                        // Does the opposite
-                        grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
-                        grabbedObject.transform.parent = null;
-                        grabbedObject = null;
-                    }
-                }
-                Debug.DrawRay(mainCamera.transform.position, mainCamera.transform.TransformDirection(Vector3.forward) * hit.distance, Color.green, 3f);
-                Debug.Log("Did Hit");
+                // Stores the hit object, reparents it to the camera, and turns off its physics
+                grabbedObject = checkedObj;
+                grabbedObject.transform.SetParent(mainCamera.transform);
+                grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
             }
-            else
+            else if (grabbedObject != null)
             {
-                Debug.DrawRay(mainCamera.transform.position, mainCamera.transform.TransformDirection(Vector3.forward) * 1000, Color.red, 3f);
-                Debug.Log("Did not Hit");
+                grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
+                grabbedObject.transform.parent = null;
+                grabbedObject = null;
             }
         }
     }
