9966f057ecee2764f69e7ce7496262b5de921c74
Ricardo Moctezuma <rmoctezumaf@gmail.com>
2024-04-28 21:43:21 -0500

    +Basic Grab Functionality

--- Assets/Scripts/PlayerInteractions.cs
+++ Assets/Scripts/PlayerInteractions.cs
@@ -1,44 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Rendering.LookDev;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace StarterAssets
 {
     public class PlayerInteractions : MonoBehaviour
     {
-        private StarterAssetsInputs _input;
-        private PlayerInput _playerInput; 
-        private CharacterController _controller;
-        private InputAction grabAction;
+        private GameObject mainCamera;
+        private GameObject grabbedObject;
 
         // Start is called before the first frame update
         void Start()
         {
-            _input = GetComponent<StarterAssetsInputs>();
-            _playerInput = GetComponent<PlayerInput>();
-            _controller = GetComponent<CharacterController>();
-            grabAction = _playerInput.actions.FindAction("Grab");
-
-            // Called when Grab is triggered
-            grabAction.performed += context => {Debug.Log("Grab triggered.");};
-        }
-
-        void OnTriggerStay(Collider other)
-        {
-            if (other.gameObject.name == "Cube" && grabAction.ReadValue<float>() == 1f)
-            {
-                Debug.Log("Grabbed cube");
-                other.enabled = false;
-            }
+            mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
         }
 
         void OnGrab()
         {
-            // Called when grab is triggered
-            Debug.Log(grabAction.ReadValue<float>());
+            GrabCheck();
         }
 
+        void GrabCheck()
+        {
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.TransformDirection(Vector3.forward), out RaycastHit hit, 3f))
+            {
+                if (hit.collider.gameObject.CompareTag("Grabbable"))
+                {
+                    if (grabbedObject == null)
+                    {
+                        // Stores the hit object, reparents it to the camera, and turns off its physics
+                        grabbedObject = hit.collider.gameObject;
+                        grabbedObject.transform.SetParent(mainCamera.transform);
+                        grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
+                    }
+                    else
+                    {
+                        // Does the opposite
+                        grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
+                        grabbedObject.transform.parent = null;
+                        grabbedObject = null;
+                    }
+                }
+                Debug.DrawRay(mainCamera.transform.position, mainCamera.transform.TransformDirection(Vector3.forward) * hit.distance, Color.green, 3f);
+                Debug.Log("Did Hit");
+            }
+            else
+            {
+                Debug.DrawRay(mainCamera.transform.position, mainCamera.transform.TransformDirection(Vector3.forward) * 1000, Color.red, 3f);
+                Debug.Log("Did not Hit");
+            }
+        }
     }
 }
