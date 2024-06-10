ae1fdefcafef678142ca23a8f2695fc62699dd1a
Ricardo Moctezuma <rmoctezumaf@gmail.com>
2024-06-10 18:33:41 -0500

    Tied AudioManager to state changes
    Also removed duplicate AudioManager in Level scene

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using FMODUnity;
 using FMOD.Studio;
 
@@ -10,7 +9,7 @@ public class AudioManager : MonoBehaviour
 
     [SerializeField]
     public EventReference bgm, pickUp, pourCoffee, bellDing, playerMove;
-    private EventInstance bgmInstance;
+    private EventInstance bgmInstance, moveInstance;
 
     private bool playerActive = false;
 
@@ -25,26 +24,55 @@ public class AudioManager : MonoBehaviour
         {
             instance = this;
         }
-        // Subscribe to the scene loaded event
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        if (scene.name == "Level")
-        {
-            playerActive = true;
-            RuntimeManager.PlayOneShot(playerMove);
-        }
     }
 
     private void Start()
     {
+        // Subscribe to the state change event
+        StateManager.Instance.OnStateChanged += HandleStateChange;
+
         bgmInstance.getPlaybackState(out PLAYBACK_STATE state);
         if (state == PLAYBACK_STATE.STOPPED)
         {
             bgmInstance = RuntimeManager.CreateInstance(bgm);
             bgmInstance.start();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // Unsubscribe from the state change event
+        StateManager.Instance.OnStateChanged -= HandleStateChange;
+    }
+
+    private void HandleStateChange(State newState)
+    {
+        if (newState.GetType() == typeof(ShiftState))
+        {
+            // Unpause the moveInstance if it was paused
+            moveInstance.setPaused(false);
+            playerActive = true;
+
+            // Spin up a new moveInstance if there currently isn't one
+            moveInstance.getPlaybackState(out PLAYBACK_STATE state);
+            if (state == PLAYBACK_STATE.STOPPED)
+            {
+                moveInstance = RuntimeManager.CreateInstance(playerMove);
+                moveInstance.start();
+            }
+        }
+        else if (newState.GetType() == typeof(PauseState))
+        {
+            // Pause the move instance in the pause state
+            playerActive = false;
+            moveInstance.setPaused(true);
+        }
+        else
+        {
+            // Completely stop the move instance in other states
+            playerActive = false;
+            moveInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            RuntimeManager.StudioSystem.setParameterByName("MoveInput", 0f, true);
         }
     }
 
