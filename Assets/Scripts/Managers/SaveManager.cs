18bc6ba679b22cd9aff6ba5c92bc794c3ccddbf9
Ricardo Moctezuma <rmoctezumaf@gmail.com>
2024-06-12 18:53:55 -0500

    Moved SaveData class into SaveManager, added money

--- Assets/Scripts/Managers/SaveManager.cs
+++ Assets/Scripts/Managers/SaveManager.cs
@@ -2,104 +2,125 @@ using UnityEngine;
 using UnityEditor;
 using System.IO;
 
-    public class SaveManager : MonoBehaviour
+[System.Serializable]
+public class SaveData
+{
+    public int currentDay = 0;
+    public string playerName = "";
+    public string cafeName = "";
+    public int highScore = 0;
+    public int dayCount = 0;
+    public int playerMoney = 0;
+}
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager instance { get; private set; }
+
+    [SerializeField]
+    private SaveData saveData = new SaveData();
+    private string path = "Assets/SaveData/saveData.json";
+
+    private void Awake()
     {
-        public static SaveManager instance { get; private set; }
-
-        [SerializeField]
-        private SaveData saveData = new SaveData();
-        private string path = "Assets/SaveData/saveData.json";
-
-        private void Awake()
+        // If there is an instance, and it's not me, delete myself.
+        if (instance != null && instance != this)
         {
-            // If there is an instance, and it's not me, delete myself.
-            if (instance != null && instance != this)
-            {
-                Destroy(this);
-            }
-            else
-            {
-                instance = this;
-            }
+            Destroy(this);
         }
-
-        void Start()
+        else
         {
-            // Create initial save data if the json doesn't exist yet
-            if (!File.Exists(path))
-            {
-                Save();
-                Debug.Log("Created initial save data");
-            }
-            // Read the json into the saveData object
-            JsonUtility.FromJsonOverwrite(File.ReadAllText(path), saveData);
-        }
-
-        [ContextMenu("Save")]
-        public void Save()
-        {
-            // Create the file path and appropriate stream and writer objects
-            FileStream stream = new(path, FileMode.Create); // The Create mode allows the program to overwrite the existing file with the same name if it exists
-            StreamWriter writer = new(stream);
-
-            // Write the data and close the objects
-            writer.WriteLine(JsonUtility.ToJson(saveData));
-            writer.Close();
-            stream.Close();
-
-            // Reimport the new json file
-            AssetDatabase.ImportAsset(path);
-            Debug.Log("Successfully saved!");
-        }
-
-        public void SetDay(int day)
-        {
-            saveData.currentDay = day;
-        }
-
-        public int GetCurrentDay()
-        {
-            return saveData.currentDay;
-        }
-
-        public void SetPlayerName(string name)
-        {
-            saveData.playerName = name;
-        }
-
-        public string GetPlayerName()
-        {
-            return saveData.playerName;
-        }
-
-        public void SetCafeName(string name)
-        {
-            saveData.cafeName = name;
-        }
-
-        public string GetCafeName()
-        {
-            return saveData.cafeName;
-        }
-
-        public void SetHighScore(int score)
-        {
-            saveData.highScore = score;
-        }
-
-        public int GetHighScore()
-        {
-            return saveData.highScore;
-        }
-
-        public void SetDayCount(int dayCount)
-        {
-            saveData.dayCount = dayCount;
-        }
-
-        public int GetDayCount()
-        {
-            return saveData.dayCount;
+            instance = this;
         }
     }
+
+    void Start()
+    {
+        // Create initial save data if the json doesn't exist yet
+        if (!File.Exists(path))
+        {
+            Save();
+            Debug.Log("Created initial save data");
+        }
+        // Read the json into the saveData object
+        JsonUtility.FromJsonOverwrite(File.ReadAllText(path), saveData);
+    }
+
+    [ContextMenu("Save")]
+    public void Save()
+    {
+        // Create the file path and appropriate stream and writer objects
+        FileStream stream = new(path, FileMode.Create); // The Create mode allows the program to overwrite the existing file with the same name if it exists
+        StreamWriter writer = new(stream);
+
+        // Write the data and close the objects
+        writer.WriteLine(JsonUtility.ToJson(saveData));
+        writer.Close();
+        stream.Close();
+
+        // Reimport the new json file
+        AssetDatabase.ImportAsset(path);
+        Debug.Log("Successfully saved!");
+    }
+
+    public void SetDay(int day)
+    {
+        saveData.currentDay = day;
+    }
+
+    public int GetCurrentDay()
+    {
+        return saveData.currentDay;
+    }
+
+    public void SetPlayerName(string name)
+    {
+        saveData.playerName = name;
+    }
+
+    public string GetPlayerName()
+    {
+        return saveData.playerName;
+    }
+
+    public void SetCafeName(string name)
+    {
+        saveData.cafeName = name;
+    }
+
+    public string GetCafeName()
+    {
+        return saveData.cafeName;
+    }
+
+    public void SetHighScore(int score)
+    {
+        saveData.highScore = score;
+    }
+
+    public int GetHighScore()
+    {
+        return saveData.highScore;
+    }
+
+    public void SetDayCount(int dayCount)
+    {
+        saveData.dayCount = dayCount;
+    }
+
+    public int GetDayCount()
+    {
+        return saveData.dayCount;
+    }
+
+    public void AdjustPlayerMoney(int money)
+    {
+        saveData.playerMoney += money;
+    }
+
+    public int GetPlayerMoney()
+    {
+        return saveData.playerMoney;
+    }
+}
 
